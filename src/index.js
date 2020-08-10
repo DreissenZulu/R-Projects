1f6d015b14dff46783badcd505dad9e9911de709
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-08-10 00:27:38 -0700

    Added basename prop to router. Also changed site name

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
